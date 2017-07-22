e2c4b4f44d356f0e7691ea91853b86661bea9259
Eduard Gafiyatullin <ehduard.gafiyatillin@gmail.com>
2017-07-22 19:49:58 +0300

    Init commit

---
+++ src/Parser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace eduardg\parser;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Parser
+ *
+ * @author Eduard
+ */
+class Parser implements ParserInterface{
+    //put your code here
+    public function process(string $url, string $tag): array {
+        return array();
+    }
+}
